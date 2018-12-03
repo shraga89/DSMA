e69af531fd872c176c32b89fb3bbd2554b2096d4
shraga89 <shraga89@gmail.com>
2018-12-03 15:37:49 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -115,11 +115,11 @@ def eval_worker(dh, X_feat, X_seq, y_single, gru_model_eval, cnn_model_eval, dnn
 
 
 E = 'f'
-dh = DH.DataHandler('../VectorsWF.csv', '../_matrix.csv', False)
+dh = DH.DataHandler('../VectorsWFtrimmed.csv', '../_matrix.csv', False)
 dh.build_eval(False)
 dh.build_feat_dataset()
 kfold = KFold(5, True, 1)
-keys = np.array(list(dh.conf_dict.keys()))[:5000]
+keys = np.array(list(dh.conf_dict.keys()))[:1500]
 # keys = np.array(random.sample(range(len(list(dh.conf_dict.keys()))), 100))
 res_adapt = pd.DataFrame(columns=['instance', 'type', 'k', 'old_p', 'old_r', 'old_f', 'new_p', 'new_r', 'new_f'])
 res_adapt_eval = pd.DataFrame(columns=['instance', 'type', 'k', 'old_p', 'old_r',
