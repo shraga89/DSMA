5bf55270e424907366328098b62ef01362ad8799
shraga89 <shraga89@gmail.com>
2018-12-06 14:55:33 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -18,6 +18,8 @@ import multiprocessing as mp
 import os
 import keras
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 # tf.Session(config=tf.ConfigProto(intra_op_parallelism_threads=28))
 # tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
@@ -28,6 +30,7 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 keras.backend.set_session(sess)
+
 
 def model_worker_adapt(model, X, y, e, b, v):
     mat = X.reshape(X.shape[1:3])
