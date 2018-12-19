6cd81bf9234eb66e26c25a462d717825085a4337
shraga89 <shraga89@gmail.com>
2018-12-19 09:20:36 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -172,7 +172,7 @@ def eval_worker(dh, X_feat, X_seq, y_single, gru_model_eval, cnn_model_eval, dnn
 
 
 print(K.tensorflow_backend._get_available_gpus())
-E = 'f'
+E = 'p'
 dataset = 'WF'
 dh = DH.DataHandler('../VectorsWFtrimmed.csv', '../_matrix.csv', False)
 dh.build_eval(False)
