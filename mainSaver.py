39e69e51f1cad6c538cfeda4b9c41e87370bfd97
shraga89 <shraga89@gmail.com>
2018-12-24 15:34:30 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -31,7 +31,7 @@ os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 # config = tf.ConfigProto(device_count={'GPU': 2, 'CPU': 2})
 config = tf.ConfigProto(device_count={'GPU': 2, 'CPU': 2})
 config.gpu_options.allow_growth = True
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
+config.gpu_options.per_process_gpu_memory_fraction = 0.95
 config.allow_soft_placement = True
 sess = tf.Session(config=config)
 keras.backend.set_session(sess)
