0bdf2acf4c949d501c1c7e56e8b99535925f8017
shraga89 <shraga89@gmail.com>
2018-12-27 09:42:07 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -29,12 +29,12 @@ os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 # sess = tf.Session(config=config)
 # keras.backend.set_session(sess)
 # config = tf.ConfigProto(device_count={'GPU': 2, 'CPU': 2})
-config = tf.ConfigProto(device_count={'GPU': 0, 'CPU': 20})
-config.gpu_options.allow_growth = True
-config.gpu_options.per_process_gpu_memory_fraction = 0.95
-config.allow_soft_placement = True
-sess = tf.Session(config=config)
-keras.backend.set_session(sess)
+# config = tf.ConfigProto(device_count={'GPU': 0, 'CPU': 20})
+# config.gpu_options.allow_growth = True
+# config.gpu_options.per_process_gpu_memory_fraction = 0.95
+# config.allow_soft_placement = True
+# sess = tf.Session(config=config)
+# keras.backend.set_session(sess)
 
 
 def model_worker_adapt(model, X, y, e, b, v):
