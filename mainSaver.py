6aec000274d0e7323dc7f8c6748226a1050118b5
shraga89 <shraga89@gmail.com>
2019-01-16 11:00:03 +0200

    update

--- mainSaver.py
+++ mainSaver.py
@@ -173,7 +173,7 @@ def eval_worker(dh, X_feat, X_seq, y_single, gru_model_eval, cnn_model_eval, dnn
 
 
 print(K.tensorflow_backend._get_available_gpus())
-E = 'f'
+E = 'cos'
 dataset = 'OAEI_noaug'
 dh = DH.DataHandler('../VectorsOAEI.csv', '../_matrix.csv', False)
 dh.build_eval(False)
