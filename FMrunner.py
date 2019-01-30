054c4b105de9ef17d734dad0e92d2efb1a226277
shraga89 <shraga89@gmail.com>
2019-01-30 15:40:31 +0200

    update

--- FMrunner.py
+++ FMrunner.py
@@ -41,7 +41,7 @@ for train, test in kfold.split(keys):
         y_single = dh.fullMat_dict[epoch][E]
 
         # print(X_mat[0].reshape(X_mat.shape[1:3]))
-        fm.fit(X_seq.reshape(X_seq.shape[1]), y_seq.reshape(y_seq.shape[1]))
+        fm.fit(X_seq.reshape(X_seq.shape[1:3]), y_seq.reshape(y_seq.shape[1]))
 
     for epoch in test:
         ts = time.time()
