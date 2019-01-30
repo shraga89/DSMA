e8f3822599c5400f95995048b2fadfa5aadddcc2
shraga89 <shraga89@gmail.com>
2019-01-30 17:57:29 +0200

    update

--- AdaptAndEvaluate.py
+++ AdaptAndEvaluate.py
@@ -629,7 +629,8 @@ def reg_adapt_fm(instance, _type, X, y, adaptor, size_m, size_n, res_adapt, coun
     yhat_full = np.array(yhat_full.reshape(len(yhat_full), 1)).round()
     print(yhat_full.dtype)
     print(y.dtype)
-    print(((yhat_full != 0.) & (yhat_full != 1.)).any())
+    print(yhat_full[(yhat_full != 1) & (yhat_full != 0)])
+    print(yhat_full[(yhat_full != 1) & (yhat_full != 0)].index)
     k_adapt = 0
     res_row_adapt = np.concatenate((instance, _type, str(k_adapt),
                                     precision_recall_fscore_support(y, np.ceil(np.array(X.reshape(yhat_full.shape))),
